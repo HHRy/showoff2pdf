20668daa6003593900d6a896736cdf5e9222a4bd
Ryan Stenhouse <ryan@ryanstenhouse.eu>
2010-11-16 22:05:10 +0000

    Convert showoff slides to PDF documents

---
+++ show_off_pdf.rb
@@ -0,0 +1,8 @@
+#!/usr/bin/env ruby
+require 'rubygems'
+require File.dirname(__FILE__) + '/lib/markdown_parser.rb'
+require File.dirname(__FILE__) + '/lib/show_off_utils.rb'
+require File.dirname(__FILE__) + '/lib/show_off_slide_stream.rb'
+require File.dirname(__FILE__) + '/lib/show_off_slide.rb'
+require 'cgi'
+require 'showoff'
