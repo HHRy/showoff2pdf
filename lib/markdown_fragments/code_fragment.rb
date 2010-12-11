b9aaf14c0312a0b9c25516238d77c1e5833cd16e
Ryan Stenhouse <ryan@ryanstenhouse.eu>
2010-12-11 18:05:51 +0000

    More sensible sizing of code fragments

--- lib/markdown_fragments/code_fragment.rb
+++ lib/markdown_fragments/code_fragment.rb
@@ -1,16 +1,15 @@
 class CodeFragment < MarkdownFragment
-  attr_accessor :ordered
   
   def render_on(pdf_object, options = {})
     arguments = _default_render_options.merge(options)
-    width = ((pdf_object.bounds.width / 100) * 90)
+    width = ((pdf_object.bounds.width / 100) * 95)
     data = []
 
     @content.each_with_index do |item, i|
       # Strip any un-needed white space
       #
       item = item.gsub(/\s\s+/,' ')
-      data << [ "<font name=\"Courier\">#{item}</font>" ]
+      data << [ "<font name=\"Courier\" size=\"10\">#{item}</font>" ]
     end
  
     pdf_object.table data, arguments.merge({:width => width}) do
@@ -18,15 +17,11 @@ class CodeFragment < MarkdownFragment
     end
     pdf_object.move_down(5) 
   end
-  
-  def ordered?
-    @ordered == true
-  end
 
   private
 
   def _default_render_options
-    options = {:cell_style =>  { :inline_format => true}}
+    options = {:cell_style =>  { :inline_format => true, :padding => 0}}  
   end  
   
 end
